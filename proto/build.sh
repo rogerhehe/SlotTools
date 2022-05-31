163ea13e5834a893e91746357971b5194d1b17ce
hejun <35733522@qq.com>
2022-05-31 11:21:40 +0800

    fix sh

--- proto/build.sh
+++ proto/build.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 protoc --go_out=. *.proto
+
+# sleep 3
+
+base_path=$(cd `dirname $0`; pwd)
+src_dir_path=$base_path"/pb"
+
+cd `dirname $0`/..
+out_dir_path=$(cd `dirname $0`; dirname $(pwd))
+out_dir_path=$out_dir_path"/SlotServer/common"
+
+cp -r $src_dir_path $out_dir_path
+rm -r $src_dir_path
