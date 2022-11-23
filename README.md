1033cfff9f1d7d42f99daac85776fbdbfaa68878
wjy <37wjy@sina.com>
2022-11-23 13:54:57 +0800

    upgrade gen config

--- README.md
+++ README.md
@@ -11,3 +11,7 @@ brew install protobuf protoc-gen-go protoc-gen-go-grpc
 添加[build.sh](build.sh)
 
 加入lua proto 生成
+
+
+gen_cev :csv生成lua和json
+gen_proto : proto 生成lua
---
+++ lua/example_player_cfg.dict.lua
@@ -0,0 +1,31 @@
+local config = 
+{
+	aaa = 
+	{
+		val = 
+		{
+			a = 111,
+			b = 33,
+		},
+		c = '4',
+	},
+	bbb = 
+	{
+		val = 
+		{
+			a = 222,
+			b = 5,
+		},
+		c = '2',
+	},
+	ccc = 
+	{
+		val = 
+		{
+			a = 333,
+			b = 3,
+		},
+		c = '5',
+	},
+}
+ return config
