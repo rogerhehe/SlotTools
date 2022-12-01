d2368bae76795f5fe7ace47ee16760f06288dfc4
hejun <35733522@qq.com>
2022-12-01 15:35:16 +0800

    fix

--- lua/player_level.lua
+++ lua/player_level.lua
@@ -6,99 +6,99 @@ local config = {
 	},
 	{
 		level = 2,
-		exp = 100,
+		exp = 200,
 	},
 	{
 		level = 3,
-		exp = 100,
+		exp = 300,
 	},
 	{
 		level = 4,
-		exp = 100,
+		exp = 400,
 	},
 	{
 		level = 5,
-		exp = 200,
+		exp = 500,
 	},
 	{
 		level = 6,
-		exp = 200,
+		exp = 600,
 	},
 	{
 		level = 7,
-		exp = 200,
+		exp = 700,
 	},
 	{
 		level = 8,
-		exp = 200,
+		exp = 800,
 	},
 	{
 		level = 9,
-		exp = 200,
+		exp = 900,
 	},
 	{
 		level = 10,
-		exp = 200,
+		exp = 1100,
 	},
 	{
 		level = 11,
-		exp = 200,
+		exp = 1200,
 	},
 	{
 		level = 12,
-		exp = 200,
+		exp = 1300,
 	},
 	{
 		level = 13,
-		exp = 200,
+		exp = 1400,
 	},
 	{
 		level = 14,
-		exp = 200,
+		exp = 1500,
 	},
 	{
 		level = 15,
-		exp = 300,
+		exp = 1600,
 	},
 	{
 		level = 16,
-		exp = 300,
+		exp = 1700,
 	},
 	{
 		level = 17,
-		exp = 300,
+		exp = 1800,
 	},
 	{
 		level = 18,
-		exp = 300,
+		exp = 1900,
 	},
 	{
 		level = 19,
-		exp = 300,
+		exp = 2000,
 	},
 	{
 		level = 20,
-		exp = 300,
+		exp = 2100,
 	},
 	{
 		level = 21,
-		exp = 300,
+		exp = 2200,
 	},
 	{
 		level = 22,
-		exp = 300,
+		exp = 2300,
 	},
 	{
 		level = 23,
-		exp = 300,
+		exp = 2400,
 	},
 	{
 		level = 24,
-		exp = 300,
+		exp = 2500,
 	},
 	{
 		level = 25,
-		exp = 300,
+		exp = 2600,
 	}
 }
 return config
