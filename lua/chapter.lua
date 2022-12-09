97b37c9a31c521d0679ece54f14e518ab4093bb2
hejun <35733522@qq.com>
2022-12-09 23:16:25 +0800

    fix

--- lua/chapter.lua
+++ lua/chapter.lua
@@ -4,16 +4,25 @@ local config = {
 		level = 1,
 		name = 'Steep Escape',
 		icon = 'chapter1',
+		bgm = 'bgm3',
 	},
 	{
 		level = 2,
 		name = 'Foothill Disorder',
 		icon = 'chapter2',
+		bgm = 'bgm2',
 	},
 	{
 		level = 3,
 		name = 'Forest Edge',
 		icon = 'chapter3',
+		bgm = 'bgm1',
+	},
+	{
+		level = 4,
+		name = 'Desert Center',
+		icon = 'chapter4',
+		bgm = 'bgm4',
 	}
 }
 return config
